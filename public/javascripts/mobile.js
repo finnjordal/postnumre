ad91bfe8fc41875e94c1d7dd1788064cd4515b92
Finn Jordal <finn@jordal.dk>
2012-06-15 15:15:32 +0200

    Mobil

---
+++ public/javascripts/mobile.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+
+  var parametre = {};
+  parametre.land = "danmark";
+  $.ajax({
+    url: 'postnumre',
+    data: parametre,
+    dataType: "json",
+    error: postnumrefejl,
+    success: vispostnumre
+  });
+
+  function vispostnumre (postnumre) {
+    $("#postnumre").empty();
+    $.each(postnumre, function (i, postnummer) {
+      var s = "<li><h3>" + postnummer.postnr + " " + postnummer.navn + "</h3>";
+      if (postnummer.gade.length > 0) s += "<p><strong>" + postnummer.gade + "</strong><p>";
+      if (postnummer.firma.length > 0) s += "<p>" + postnummer.firma + "<p>";
+      $("#postnumre").append(s);
+     
+    });
+    $('#postnumre').listview();
+    $('#postnumre').listview('refresh');
+  };
+
+
+  function postnumrefejl (xhr, status, errorThrown) {
+    alert("Fejl ved hentning af postnumre: " + xhr.status);
+  };
+
+});
